bf4f53342ace1e241e355e125779ecef33937d2f
Kong To <newlight77@gmail.com>
2022-07-07 23:44:38 +0200

    feature(nvim): cleaner customize.sh script

--- customize.sh
+++ customize.sh
@@ -7,23 +7,21 @@ if [ ! -d "$DIR" ]; then DIR="$PWD"; fi
 
 echo "*** ------  Customize ------ ***" 1>&2
 
-rm -rf $HOME/.config/nvim
+# rm -rf $HOME/.config/nvim
 rm -rf $HOME/.local/share/nvim
 rm -rf $HOME/.cache/nvim
 
 DIR=$HOME/.config/nvim
 
-if [ -d $HOME/.config/nvim  ]; then
+if [ -d "$DIR" ]; then
   echo "making backup of .config/nvim" 1>&2
-  mv $HOME/.config/nvim           $HOME/.config/nvim.$(date +"%Y%m%d%H%M%S")
+  mv $DIR           $DIR.$(date +"%Y%m%d%H%M%S")
 fi
 
 
 if [ ! -d "$DIR" ]; then
   echo "cloning nvim-preconfigured to $DIR" 1>&2
   git clone https://github.com/newlight77/neovim-preconfigured.git $DIR
-else
-  cd $DIR && git pull origin main
 fi
 
 
